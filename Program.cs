fc0101c37c3395fe4bf452a15e2b553829d91c66
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-12 20:43:08 -0300

    termino com nome do usuario

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome...");
+            string nomeUsuario = Console.ReadLine();
+            Console.Write("Olá, ");
+            Console.WriteLine(nomeUsuario);
+
+        }
+    }
+}
